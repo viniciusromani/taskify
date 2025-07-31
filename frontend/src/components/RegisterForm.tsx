8da60fd30986df061e7f168fa5c9cda6cd8a4e2c
Vinicius Romani <vn.romani@gmail.com>
2025-07-31 16:30:25 -0300

    adding register page and form - similar to login

---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,111 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { z } from "zod";
+
+import { Button } from "@/components/shadcn/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/shadcn/form";
+import { Input } from "@/components/shadcn/input";
+import { register } from "@/services/auth";
+
+const schema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Nome tem que ter no mínimo 2 caracteres" })
+    .max(70, { message: "Nome não pode ser maior que 70 caracteres" })
+    .regex(/^[a-zA-Z\s.-]+$/, {
+      message: "Nome pode ter apenas letras, espaço, hífen e ponto-final.",
+    }),
+  email: z.email("Email inválido"),
+  password: z.string().min(3, "Senha tem que ter no mínimo 3 caracteres"),
+});
+type RegisterData = z.infer<typeof schema>;
+
+function RegisterForm({
+  onRegisterSuccess,
+}: {
+  onRegisterSuccess: () => void;
+}) {
+  const form = useForm<RegisterData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: RegisterData) => mutation.mutate(data);
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: onRegisterSuccess,
+  });
+
+  if (mutation.isError) {
+    // TODO: improve error handling and error messages
+    toast.error(mutation.error.message);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input placeholder="Entre seu nome" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Entre seu email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Entre sua senha"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full" isLoading={mutation.isPending}>
+          Criar conta
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export { RegisterForm };
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,42 @@
+import { User } from "lucide-react";
+import { useNavigate } from "react-router";
+
+import { RegisterForm } from "@/components/RegisterForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcn/card";
+
 function Register() {
-  return <div>Register Page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col flex-1 my-8 mx-4 items-center space-y-8">
+      <div className="w-12 h-12 bg-primary rounded-xl flex items-center justify-center mb-4">
+        <User className="w-6 h-6 text-primary-foreground" />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl font-bold text-foreground">Crie sua conta</h1>
+        <p className="text-muted-foreground text-center">
+          Junte-se a nós hoje e registre-se em segundos
+        </p>
+      </div>
+      <Card className="w-full sm:max-w-xl">
+        <CardHeader className="flex flex-col items-center">
+          <CardTitle>Registrar</CardTitle>
+          <CardDescription className="text-center">
+            Forneça suas informações para criar sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <RegisterForm onRegisterSuccess={() => navigate("/login")} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default Register;
