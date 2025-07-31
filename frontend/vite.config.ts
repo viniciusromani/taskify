835e8a21cbb25a630f117eadbead9057b19b9eb4
Vinicius Romani <vn.romani@gmail.com>
2025-07-31 14:38:05 -0300

    adding shadcn and tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
